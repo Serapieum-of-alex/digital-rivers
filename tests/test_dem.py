20b9be9c41049b9f1d4774f55cc1b2c50b0c7ea8
Mostafa Farrag <moah.farag@gmail.com>
2025-08-24 23:01:46 +0200

    add tests

--- tests/test_dem.py
+++ tests/test_dem.py
@@ -109,23 +109,3 @@ def test_flow_direction_array_cells_indices(
     assert isinstance(fd_cell, np.ndarray)
     assert fd_cell.shape == (dem.rows, dem.columns, 2)
     assert np.array_equal(fd_cell, flow_direction_array_cells_indices, equal_nan=True)
-
-
-def test_flowDirectionIndex(coello_df_4000: gdal.Dataset):
-    dem = DEM(coello_df_4000)
-    fd_cell = dem.flow_direction_index()
-    assert isinstance(fd_cell, np.ndarray)
-    assert fd_cell.shape == (dem.rows, dem.columns, 2)
-
-
-def test_flowDirectionTable(coello_df_4000: gdal.Dataset):
-    dem = DEM(coello_df_4000)
-    fd_cell = dem.flow_direction_table()
-    assert isinstance(fd_cell, np.ndarray)
-    assert fd_cell.shape == (dem.rows, dem.columns, 2)
-
-
-def test_flowDirectionTable(coello_df_4000: gdal.Dataset, coello_fdt):
-    dem = DEM(coello_df_4000)
-    fd_table = dem.flow_direction_table()
-    assert fd_table == coello_fdt
